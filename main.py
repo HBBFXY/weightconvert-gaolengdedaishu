050e91f831def1c3b64e00e923da04979acc2eb2
gaolengdedaishu <1942806425@qq.com>
2025-09-08 16:46:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
-input(f"输入重量{zhonglilang}")
-if zhongliang.[-1] = kg
-  print(f"这个重量{zhongliang*2.2046}")
-elif zhongliang.[-1] = pd
-  print(f"这个重量是{zhongliang/2.2046}")
-else
-  print("兄弟，带上单位，kg还是pd")
+zhongliang = input("输入重量")
+num = float(zhongliang[:-2])
+if zhongliang[-2:] == "kg" :
+    print(f"这个重量{num*2.2046}")
+elif zhongliang[-2:] == "pd" :
+    print(f"这个重量是{num/2.2046}")
+else :
+    print("兄弟，带上单位，kg还是pd")
