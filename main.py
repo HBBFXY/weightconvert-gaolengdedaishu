d74ece07971bf2b0d08efbae2932864e6376b350
gaolengdedaishu <1942806425@qq.com>
2025-09-08 17:00:09 +0800

    Update main.py
    完成转换作业

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 zhongliang = input("输入重量")
 num = float(zhongliang[:-2])
 if zhongliang[-2:] == "kg" :
-    print(f"这个重量{num*2.2046}")
+    jieguo = num * 2.2046
+    print(f"这个重量{jieguo:.3f}")
 elif zhongliang[-2:] == "pd" :
-    print(f"这个重量是{num/2.2046}")
+    jieguo = num/2.2046
+    print(f"这个重量是{jieguo:.3f}")
 else :
     print("兄弟，带上单位，kg还是pd")
 # 老师这肯定没错啊
