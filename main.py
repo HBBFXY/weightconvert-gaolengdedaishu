1e32eb7ee170f7a35e2a266f7b97c866b3eaadc2
gaolengdedaishu <1942806425@qq.com>
2025-09-08 16:35:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
+input(f"输入重量{zhonglilang}")
+if zhongliang.[-1] = kg
+  print(f"这个重量{zhongliang*2.2046}")
+elif zhongliang.[-1] = pd
+  print(f"这个重量是{zhongliang/2.2046}")
+else
+  print("兄弟，带上单位，kg还是pd")
